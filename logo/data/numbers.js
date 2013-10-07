9b91c6d1a86c90fd06c6a93fc7df1c1429ea1242
shah- <artashes.shahinyan@gmail.com>
2013-10-07 14:09:50 +0400

    Image file names array for numbers game

--- logo/data/numbers.js
+++ logo/data/numbers.js
@@ -1,23 +1,25 @@
-Խնձոր
-Բանան
-Մեղու-
-Գիրք
-Գլխարկ
-Կատու-
-Աթոռ
-Պանիր
-Շուն
-Ձու
-Կիթառ
-Մուրճ
-Կիտրոն
-Դեղձ
-Տանձ
-Մատիտ-
-Խոզ
-Պարան
-Ելակ-
-Շապիկ-
-Թեյ
-Ծառ
-Ջութակ
+var g_numFiles = [
+  "apple.jpg",
+  "banana.jpg",
+  "bee.jpg",
+  "book.jpg",
+  "cap.jpg",
+  "cat.jpg",
+  "chear.jpg",
+  "cheese.jpg"
+  "dog.jpg",
+  "egg.jpg",
+  "guitar.jpg",
+  "hammer.jpg",
+  "lemo.jpg",
+  "peach.jpg",
+  "pear.jpg",
+  "pencil.jpg",
+  "pig.jpg",
+  "rope.jpg,
+  "strawberry.jpg",
+  "t-shirt.jpg",
+  "tea.jpg",
+  "tree.jpg",
+  "violin.jpg"
+  ];
